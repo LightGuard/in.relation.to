4e9d6a7c84aeff34d819ce9d4eefe481f41a8130
Emmanuel Bernard <emmanuel@hibernate.org>
2013-01-04 17:44:44 +0100

    Add tag filtering particularly on feed generation to gain speed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -9,13 +9,17 @@ require 'posts'
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::DataDir.new
   extension Awestruct::Extensions::Posts.new( '', :posts ) 
-  extension Awestruct::Extensions::SplitFilterer.new( :posts, 'tags', ['author', 'authors', 'tags', 'tag'] ) 
-  extension Awestruct::Extensions::Paginator.new( :posts, 'index', :per_page=>10 )
+  extension Awestruct::Extensions::SplitFilterer.new( :posts, 'tags', 
+                                                     :sanitize=>true,
+                                                     :blacklist=>['author', 'authors', 'tags', 'tag',
+                                                      'page', 'javascripts', 'images', 'readme', 'templates']
+                                                    ) 
+  extension Awestruct::Extensions::Paginator.new( :posts, 'index', :per_page=>30, :per_page_init=>5 )
   extension Awestruct::Extensions::Splitter.new( :posts, 
                                                  'tags',
                                                  'templates/tags', 
                                                  '/', 
-                                                 :per_page=>10,
+                                                 :per_page=>30, :per_page_init=>5,
                                                  :output_home_file=>'index',
                                                  :sanitize=>true )
   extension Awestruct::Extensions::SplitCloud.new( :posts,
@@ -27,24 +31,30 @@ Awestruct::Extensions::Pipeline.new do
                                                  'author',
                                                  'templates/author', 
                                                  '/', 
-                                                 :per_page=>10,
+                                                 :per_page=>30, :per_page_init=>5,
                                                  :output_home_file=>'index',
                                                  :sanitize=>true )
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::SplitAtomizer.new( :posts, 
-                                                 'tags',
-                                                 '/', 
-                                                 :num_entries=>10,
-                                                 :sanitize=>true )
+                                                     'tags',
+                                                     '/', 
+                                                     :num_entries=>10,
+                                                     :sanitize=>true,
+                                                     :whitelist=>['announcement', 'bean-validation', 'hibernate',
+                                                      'hibernate-ogm', 'hibernate-search', 'hibernate-validator',
+                                                      'jboss-tools', 'news', 'ogm', 'seam', 'weld', 'web-beans'],
+                                                     :blacklist=>['author', 'authors', 'tags', 'tag',
+                                                      'page', 'javascripts', 'images', 'readme', 'templates'])
   extension Awestruct::Extensions::SplitAtomizer.new( :posts, 
-                                                 'author',
-                                                 '/', 
-                                                 :num_entries=>10,
-                                                 :sanitize=>true )
+                                                   'author',
+                                                   '/', 
+                                                   :num_entries=>10,
+                                                   :sanitize=>true,
+                                                   :blacklist=>['sergey-smirnov'] )
   extension Awestruct::Extensions::Atomizer.new( 
     :posts, 
     '/feed.atom', 
-    :num_entries=>10000,
+    :num_entries=>100,
     :content_url=>'http://in.relation.to',
     :feed_title=> 'In Relation To' )
 end
