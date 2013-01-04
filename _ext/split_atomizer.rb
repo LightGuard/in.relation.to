741c2e0a13c8a11c30923142a989bcf46828aae2
Emmanuel Bernard <emmanuel@hibernate.org>
2013-01-04 17:44:22 +0100

    Add ability to filter split values with white and black lists

--- _ext/split_atomizer.rb
+++ _ext/split_atomizer.rb
@@ -13,14 +13,21 @@ module Awestruct
 
       def execute(site)
         @splits = site.send( "#{@split_items_property}_#{@split_property}" )
+        filters = site.splitter_filters
+        filter_property = filters.send( @split_property ) if not filters.nil?
+        whitelist = (filter_property.whitelist_atom if not filter_property.nil?) || Array.new()
+        blacklist = (filter_property.blacklist_atom if not filter_property.nil?) || Array.new()
         @splits.each do |split|
           ## Optionally sanitize split URL
-          output_prefix = File.join( @output_path, sanitize(split.to_s) )
-          atomizer = Awestruct::Extensions::Atomizer.new( @split_items_property, 
+          sanitized = sanitize(split.to_s)
+          if ( whitelist.empty? or whitelist.include? sanitized ) and not blacklist.include? sanitized
+            output_prefix = File.join( @output_path, sanitized )
+            atomizer = Awestruct::Extensions::Atomizer.new( @split_items_property, 
                                                          File.join(output_prefix, @atom_feed_file),
                                                          :content_url=>"#{site.base_url}#{output_prefix}",
                                                          :feed_title=>"In Relation To #{split.to_s}" )
-          atomizer.execute( site )
+            atomizer.execute( site )
+          end
         end
       end
 
--- _ext/split_filterer.rb
+++ _ext/split_filterer.rb
@@ -9,13 +9,13 @@ module Awestruct
     class SplitFilterer
       # split_items_property: property containing the posts
       # split_property: name of the property to split by
-      # denied_splits: array of string values forbidden
-      def initialize(split_items_property, split_property, denied_splits, opts={})
+      def initialize(split_items_property, split_property, opts={})
         @split_items_property = split_items_property
         @split_property = split_property
-        @denied_splits = Array( denied_splits )
         @sanitize = opts[:sanitize] || false
         @pagination_opts = opts
+        #@whitelist = Array( opts[:whitelist] )
+        #@blacklist = Array( opts[:blacklist] )
       end
 
       def execute(site)
@@ -25,18 +25,34 @@ module Awestruct
 
         plural_split_property =  @split_property
 
+        filters = site.splitter_filters
+        filter_property = filters.send( @split_property ) if not filters.nil?
+        whitelist = (filter_property.whitelist if not filter_property.nil?) || Array.new()
+        blacklist = (filter_property.blacklist if not filter_property.nil?) || Array.new()
+
         all.each do |page|
           splits = page.send( @split_property )
+          #to_s.urlize({:convert_spaces=>true})
           if ( splits && ! splits.empty? )
-            filtered_out_splits = splits.find_all { |split| @denied_splits.include? split.to_s }
+            filtered_out_splits = splits.find_all { |split| blacklist.include? sanitize(split) }
             splits.each do |elt|
-              if @denied_splits.include? elt.to_s
+              if blacklist.include? elt.to_s
                 puts ">>>> WARNING: tag '#{elt.to_s}' is illegal, remove it from: #{page.relative_source_path}"
               end
             end
-            filtered_splits = splits.find_all { |split| not (@denied_splits.include? split.to_s) }
+            filtered_splits = splits.find_all { 
+              |split| ( ( whitelist.empty? or whitelist.include? sanitize(split) ) and not (blacklist.include? sanitize(split) ) )
+            }
             page.send( "#{@split_property}=", filtered_splits )
           end
+        end
+      end
+
+      def sanitize(data)
+        if @sanitize
+          return data.to_s.urlize({:convert_spaces=>true})
+        else
+          return data.to_s
         end
       end
     end
